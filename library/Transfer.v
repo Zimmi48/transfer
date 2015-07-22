4780ead6f335bfac5cdd4b9ec61c542f99d0240e
Theo Zimmermann <theo.zimmermann@ens.fr>
2015-07-22 14:11:27 +0200

    add licence header

--- library/Transfer.v
+++ library/Transfer.v
@@ -1,3 +1,10 @@
+(* Apply a theorem modulo isomorphism *)
+(* Copyright Théo Zimmermann 2015
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v.2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *)
+
 Require Import Program.Basics.
 
 Definition respectful_arrow
