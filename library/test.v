1828e14820a34e52538858b5a1a5e0e3d11d1ef3
Theo Zimmermann <theo.zimmermann@ens.fr>
2016-02-22 15:15:34 +0100

    factorize

--- library/test.v
+++ library/test.v
@@ -1,6 +1,17 @@
-Require Import Utf8.
 Require Import PeanoNat.
 Require Import NArithTransfer.
+
+Lemma half_total_predicate :
+  forall {A B : Type} (R : A -> B -> Prop),
+    (forall (x : A) (x' y' : B), R x x' -> R x y' -> x' = y')
+    -> forall P' : B -> Prop, exists P : A -> Prop, (R ##> iff) P P'.
+Proof.
+  intros A B R Hfun P'.
+  exists (fun x => forall x', R x x' -> P' x').
+(*  exists (fun x => exists x', R x x' /\ P' x'). *)
+  intros; split; firstorder.
+  erewrite Hfun; eauto.
+Qed.  
 
 Instance total_predicate
   (A B : Type)
@@ -10,25 +21,12 @@ Instance total_predicate
 Proof.
   destruct (full_uniq_decl_recip _ _ _ prf) as [ Hfun Hinj ].
   split; apply full_tot_decl; split.
-  + intros P'.
-(*  exists (fun x => exists x', R x x' /\ P' x'). *)
-    exists (fun x => forall x', R x x' -> P' x').
-    intros; split; firstorder.
-    erewrite Hfun; eauto.
-  + intros P.
-    exists (fun x' => exists x, R x x' /\ P x).
-(*  exists (fun x' => forall x, R x x' -> P x). *)
-    intros; split; firstorder.
-    erewrite Hinj; eauto.
+  + exact (half_total_predicate _ Hfun).
+  + intros *.
+    edestruct (half_total_predicate (flip R)) as (P' & HP'); [ intros; eapply Hinj; eauto |].
+    exists P'.
+    intros; split; apply HP'; assumption.
 Qed.
-
-Lemma total_predicate_recip :
-  forall (A B : Type) (R : A -> B -> Prop),
-  (((R ##> iff) ##> iff) ##> iff) (@all (A -> Prop)) (@all (B -> Prop)) ->
-  ((R ##> iff) ##> (R ##> iff) ##> iff) eq eq ->
-  (R ##> R ##> iff) eq eq.
-Proof.
-Abort.
 
 Theorem N_nat_ind : forall P : N -> Prop, P 0%N -> (forall n : N, P n -> P (N.succ n)) -> forall n : N, P n.
 Proof.
