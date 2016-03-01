665372b458b368f4a016ef972b908cbed2b15033
Theo Zimmermann <theo.zimmermann@ens.fr>
2016-03-01 19:01:03 +0100

    define bitotal and biunique

--- Transfer.v
+++ Transfer.v
@@ -293,61 +293,32 @@ Proof.
     now exists x.
 Qed.
 
-Require Import Coq.Classes.Equivalence.
-Require Coq.Setoids.Setoid.
-
-Generalizable Variables eqA eqB.
-Definition biunique {A B : Type}
-           `{Equivalence A eqA} `{Equivalence B eqB}
-           (R : A -> B -> Prop) :=
-  (R ##> R ##> iff) eqA eqB.
+Definition biunique {A B : Type} (R : A -> B -> Prop) :=
+  (R ##> R ##> iff) eq eq.
 
 Theorem biunique_decl :
-  forall {A B : Type}
-    `{Equivalence A eqA} `{Equivalence B eqB}
-    (R : A -> B -> Prop),
-    (forall x x' y', R x x' -> R x y' -> eqB x' y') ->
-    (forall x y y', R x y' -> R y y' -> eqA x y) ->
+  forall (A B : Type) (R : A -> B -> Prop),
+    (forall x x' y', R x x' -> R x y' -> x' = y') ->
+    (forall x y y', R x y' -> R y y' -> x = y) ->
     biunique R.
 Proof.
-  intros * Hfun Hinj x x' relx y y' rely.
+  intros A B R Hfun Hinj x x' relx y y' rely.
   split; intro eq.
   + apply (Hfun x); trivial.
-    rewrite eq.
+    now rewrite eq.
   + apply (Hinj x y y'); trivial.
     now rewrite <- eq.
 Qed.
 
 Theorem biunique_decl_recip :
-  forall (A B : Type)
-    (eqA : A -> A -> Prop) (eqB : B -> B -> Prop) (R : A -> B -> Prop),
-    biunique eqA eqB R ->
-    (forall x x' y', R x x' -> R x y' -> eqB x' y') /\
-    (forall x y y', R x y' -> R y y' -> eqA x y).
+  forall (A B : Type) (R : A -> B -> Prop),
+    biunique R ->
+    (forall x x' y', R x x' -> R x y' -> x' = y') /\
+    (forall x y y', R x y' -> R y y' -> x = y).
 Proof.
   intros A B R Huniq; lazy beta delta in Huniq; split; intros * rel1 rel2.
   + generalize (eq_refl x).
     now apply Huniq.
   + generalize (eq_refl y').
     now apply Huniq.
-Qed.
-
-Theorem biunique_fun :
-  forall (A B C D: Type) (R : A -> B -> Prop) (S : C -> D -> Prop),
-    bitotal R -> biunique eq eq S -> biunique (eq ##> eq) (eq ##> eq) (R ##> S).
-Proof.
-  intros A B C D R S H1 H2.
-  apply bitotal_decl_recip in H1.
-  apply biunique_decl_recip in H2.
-  apply biunique_decl.
-  + intros f f' g' relf relfg.
-  + intro relPQ.
-    apply (half_uniq_predicate _ _ _ Hsurj P Q P' Q'); trivial.
-  + intro relPQ.
-    apply (half_uniq_predicate _ _ _ Htot P' Q' P Q); trivial; intros x' x relx. {
-      symmetry.
-      now apply relP.
-    }
-    apply relQ in relx.
-    now rewrite relx.
 Qed.
