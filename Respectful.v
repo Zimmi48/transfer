87a67c5d3a10bc929f704169dd6b27d34f032bc8
Theo Zimmermann <theo.zimmermann@ens.fr>
2016-03-29 13:30:07 +0200

    section Declarations

--- Respectful.v
+++ Respectful.v
@@ -20,167 +20,169 @@ Notation " R ##> R' " := (respectful_arrow R R')
 
 Local Notation " A <-> B " := (iffT A B) : type_scope.
 
-(** Totality declarations *)
+Section Declarations.
 
-(** Surjectivity, i.e. right-totality *)
-Definition righttotal {A B : Type} (R : A -> B -> Type) :=
-  ((R ##> arrow) ##> arrow) forall_def forall_def.
+  Set Implicit Arguments.
+  Variables A B : Type.
+  Variable R : A -> B -> Type.
 
-(** Left-totality *)
-Definition lefttotal {A B : Type} (R : A -> B -> Type) :=
-  ((R ##> flip arrow) ##> flip arrow) forall_def forall_def.
+  (** * Totality declarations *)
 
-(** Both right and left-totality *)
-Definition bitotal {A B : Type} (R : A -> B -> Type) :=
-  ((R ##> iffT) ##> iffT) forall_def forall_def.
+  (** Surjectivity, i.e. right-totality *)
+  Definition righttotal := ((R ##> arrow) ##> arrow) forall_def forall_def.
 
-Theorem righttotal_decl :
-  forall (A B : Type) (R : A -> B -> Type),
-    (forall x' : B, { x : A & R x x'}) <-> righttotal R.
-Proof.
-  intros A B R.
-  lazy delta zeta.
-  split; intros Hsurj.
-  - intros p p' Hp Hall x'.
-    destruct (Hsurj x') as [x Rx].
-    now apply (Hp x _).
-  - apply (Hsurj (fun _ => True) _); trivial.
-    intros x x' Rx _.
-    now exists x.
-Qed.
+  (** Left-totality *)
+  Definition lefttotal := ((R ##> flip arrow) ##> flip arrow) forall_def forall_def.
 
-Theorem lefttotal_decl :
-  forall (A B : Type) (R : A -> B -> Type),
-    (forall x : A, { x' : B & R x x'}) <-> lefttotal R.
-Proof.
-  intros A B R.
-  lazy delta zeta.
-  split; intros Htot.
-  - intros p p' Hp Hall x.
-    destruct (Htot x) as [x' Rx].
-    now apply (Hp _ x').
-  - apply (Htot _ (fun _ => True)); trivial.
-    intros x x' Rx _.
-    now exists x'.
-Qed.
+  (** Both right and left-totality *)
+  Definition bitotal := ((R ##> iffT) ##> iffT) forall_def forall_def.
 
-Lemma bitotal_from_left_and_right_total :
-  forall (A B : Type) (R : A -> B -> Type),
-    righttotal R -> lefttotal R -> bitotal R.
-Proof.
-  intros A B R.
-  lazy beta delta.
-  intros H1 H2 P P' H3.
-  split.
-  1:apply H1.
-  2:apply H2.
-  all:apply H3.
-Qed.
+  Theorem righttotal_decl : (forall x' : B, { x : A & R x x'}) <-> righttotal.
+  Proof.
+    lazy delta zeta.
+    split; intros Hsurj.
+    - intros p p' Hp Hall x'.
+      destruct (Hsurj x') as [x Rx].
+      now apply (Hp x _).
+    - apply (Hsurj (fun _ => True) _); trivial.
+      intros x x' Rx _.
+      now exists x.
+  Qed.
 
-(* The proof could maybe be made more generic with a little bit of work on
- intersection and union of relations and their compatibility with ##> *)
+  Theorem lefttotal_decl : (forall x : A, { x' : B & R x x'}) <-> lefttotal.
+  Proof.
+    lazy delta zeta.
+    split; intros Htot.
+    - intros p p' Hp Hall x.
+      destruct (Htot x) as [x' Rx].
+      now apply (Hp _ x').
+    - apply (Htot _ (fun _ => True)); trivial.
+      intros x x' Rx _.
+      now exists x'.
+  Qed.
 
-Theorem bitotal_decl :
-  forall (A B : Type) (R : A -> B -> Type),
+  Lemma bitotal_from_left_and_right_total : righttotal -> lefttotal -> bitotal.
+  Proof.
+    lazy beta delta.
+    intros H1 H2 P P' H3.
+    split.
+    1:apply H1.
+    2:apply H2.
+    all:apply H3.
+  Qed.
+
+  (* The proof could maybe be made more generic with a little bit of work on
+     intersection and union of relations and their compatibility with ##> *)
+
+  Theorem bitotal_decl :
     (forall x' : B, { x : A & R x x'}) ->
     (forall x : A, { x' : B & R x x'}) ->
-    bitotal R.
-Proof.
-  intros A B R H1 H2.
-  apply righttotal_decl in H1.
-  apply lefttotal_decl in H2.
-  now apply bitotal_from_left_and_right_total.
-Qed.
+    bitotal.
+  Proof.
+    intros righttotal lefttotal.
+    apply righttotal_decl in righttotal.
+    apply lefttotal_decl in lefttotal.
+    now apply bitotal_from_left_and_right_total.
+  Qed.
 
-Theorem bitotal_decl_recip1 :
-  forall (A B : Type) (R : A -> B -> Type),
-    bitotal R ->
-    (forall x' : B, { x : A & R x x' }).
-Proof.
-  intros * H; unfold respectful_arrow in H; intros x'.
-  apply (H (fun _ => True) (fun x' => { x : A & R x x'})); firstorder.
-Qed.
+  Lemma bitotal_decl_recip1 : bitotal -> (forall x' : B, { x : A & R x x' }).
+  Proof.
+    intros * H; unfold respectful_arrow in H; intros x'.
+    apply (H (fun _ => True) (fun x' => { x : A & R x x'})); firstorder.
+  Qed.
 
-Theorem bitotal_decl_recip2 :
-  forall (A B : Type) (R : A -> B -> Type),
-    bitotal R ->
-    (forall x : A, { x' : B & R x x' }).
-Proof.
-  intros * H; unfold respectful_arrow in H; intros x.
-  apply (H (fun x => { x' : B & R x x'}) (fun x' => True)); firstorder.
-Qed.
+  Lemma righttotal_from_bitotal : bitotal -> righttotal.
+  Proof.
+    intro bitotal.
+    apply righttotal_decl.
+    apply bitotal_decl_recip1.
+    exact bitotal.
+  Qed.
 
-Definition ball {A : Type} (subType : A -> Prop) (predicate : A -> Prop) :=
-  forall x, subType x -> predicate x.
+  Lemma bitotal_decl_recip2 : bitotal -> (forall x : A, { x' : B & R x x' }).
+  Proof.
+    intros * H; unfold respectful_arrow in H; intros x.
+    apply (H (fun x => { x' : B & R x x'}) (fun x' => True)); firstorder.
+  Qed.
 
-Theorem generic_covered_decl :
-  forall (A B : Type) (R : A -> B -> Prop),
-    let coveredA := fun x => exists y, R x y in
-    let coveredB := fun y => exists x, R x y in
-    ((R ##> iff) ##> iff) (ball coveredA) (ball coveredB).
-Proof.
-  intros A B R coveredA coveredB.
-  lazy beta delta.
-  intros P P' Prel; split.
-  + intros HP x' (x & xrel).
-    destruct (Prel x x' xrel) as [Prel' _].
-    apply Prel'.
-    apply HP.
-    now exists x'.
-  + intros HP' x (x' & xrel).
-    destruct (Prel x x' xrel) as [_ Prel'].
-    apply Prel'.
-    apply HP'.
-    now exists x.
-Qed.
+  Lemma lefttotal_from_bitotal : bitotal -> lefttotal.
+  Proof.
+    intro bitotal.
+    apply lefttotal_decl.
+    apply bitotal_decl_recip2.
+    exact bitotal.
+  Qed.
 
-Definition biunique {A B : Type} (R : A -> B -> Type) :=
-  (R ##> R ##> iffT) eq eq.
+  (** ** Generic property for non-total relations *)
+  Definition ball {A : Type} (subType : A -> Type) (predicate : A -> Type) :=
+    forall x, subType x -> predicate x.
 
-Theorem biunique_decl :
-  forall (A B : Type) (R : A -> B -> Type),
+  Theorem generic_covered_decl :
+    let coveredA := fun x => { y : B & R x y } in
+    let coveredB := fun y => { x : A & R x y } in
+    ((R ##> iffT) ##> iffT) (ball coveredA) (ball coveredB).
+  Proof.
+    intros coveredA coveredB.
+    lazy beta delta.
+    intros P P' Prel; split.
+    - intros HP x' (x & xrel).
+      destruct (Prel x x' xrel) as [Prel' _].
+      apply Prel'.
+      apply HP.
+      now exists x'.
+    - intros HP' x (x' & xrel).
+      destruct (Prel x x' xrel) as [_ Prel'].
+      apply Prel'.
+      apply HP'.
+      now exists x.
+  Qed.
+
+  Definition rightunique := (R ##> R ##> arrow) eq eq.
+  Definition biunique := (R ##> R ##> iffT) eq eq.
+
+  Theorem biunique_decl :
     (forall x x' y', R x x' -> R x y' -> x' = y') ->
     (forall x y y', R x y' -> R y y' -> x = y) ->
-    biunique R.
-Proof.
-  intros A B R Hfun Hinj x x' relx y y' rely.
-  split; intro eq.
-  + apply (Hfun x); trivial.
-    now rewrite eq.
-  + apply (Hinj x y y'); trivial.
-    now rewrite <- eq.
-Qed.
+    biunique.
+  Proof.
+    intros Hfun Hinj x x' relx y y' rely.
+    split; intro eq.
+    + apply (Hfun x); trivial.
+      now rewrite eq.
+    + apply (Hinj x y y'); trivial.
+      now rewrite <- eq.
+  Qed.
 
-Theorem biunique_decl_recip1 :
-  forall (A B : Type) (R : A -> B -> Type),
-    biunique R ->
+  Theorem biunique_decl_recip1 :
+    biunique ->
     (forall x x' y', R x x' -> R x y' -> x' = y').
-Proof.
-  intros A B R Huniq; lazy beta delta in Huniq; intros * rel1 rel2.
-  generalize (eq_refl x).
-  now apply Huniq.
-Qed.
+  Proof.
+    intros Huniq; lazy beta delta in Huniq; intros * rel1 rel2.
+    generalize (eq_refl x).
+    now apply Huniq.
+  Qed.
 
-Theorem biunique_decl_recip2 :
-  forall (A B : Type) (R : A -> B -> Type),
-    biunique R ->
+  Theorem biunique_decl_recip2 :
+    biunique ->
     (forall x y y', R x y' -> R y y' -> x = y).
-Proof.
-  intros A B R Huniq; lazy beta delta in Huniq; intros * rel1 rel2.
-  generalize (eq_refl y').
-  now apply Huniq.
-Qed.
-Lemma half_total_predicate :
-  forall {A B : Type} (R : A -> B -> Type),
+  Proof.
+    intros Huniq; lazy beta delta in Huniq; intros * rel1 rel2.
+    generalize (eq_refl y').
+    now apply Huniq.
+  Qed.
+
+  Lemma half_total_predicate :
     (forall (x : A) (x' y' : B), R x x' -> R x y' -> x' = y')
     -> forall P' : B -> Type, { P : A -> Type & (R ##> iffT) P P' }.
-Proof.
-  intros A B R Hfun P'.
-  exists (fun x => forall x', R x x' -> P' x').
-(*  exists (fun x => { x' : B & prodP (R x x') (P' x') }). *)
-  split; unfold arrow; firstorder.
-  erewrite Hfun; eauto.
-Qed.
+  Proof.
+    intros Hfun P'.
+    exists (fun x => forall x', R x x' -> P' x').
+    (*  exists (fun x => { x' : B & prodP (R x x') (P' x') }). *)
+    split; unfold arrow; firstorder.
+    erewrite Hfun; eauto.
+  Qed.
+
+End Declarations.
 
 Lemma half_total_predicate_prop :
   forall {A B : Type} (R : A -> B -> Prop),
@@ -228,13 +230,13 @@ Lemma half_uniq_predicate_inst (A B : Type) (R : A -> B -> Prop) :
   ((R ##> iff) ##> (R ##> iff) ##> iffT) (eq ##> iff) (eq ##> iff).
 Proof.
   intro H.
-  pose (Hsurj := bitotal_decl_recip1 _ _ _ H).
-  pose (Htot := bitotal_decl_recip2 _ _ _ H).
+  pose (Hsurj := @bitotal_decl_recip1 _ _ _ H).
+  pose (Htot := @bitotal_decl_recip2 _ _ _ H).
   intros P P' relP Q Q' relQ; split.
   + intro relPQ.
-    apply (half_uniq_predicate _ _ _ Hsurj P Q P' Q'); trivial.
+    apply (@half_uniq_predicate _ _ _ Hsurj P Q P' Q'); trivial.
   + intro relPQ.
-    apply (half_uniq_predicate _ _ _ Htot P' Q' P Q); trivial; intros x' x relx. {
+    apply (@half_uniq_predicate _ _ _ Htot P' Q' P Q); trivial; intros x' x relx. {
       symmetry.
       now apply relP.
     }
@@ -258,8 +260,8 @@ Lemma total_predicate
   (is_related : (R ##> R ##> iffT) eq eq) :
   (((R ##> iffT) ##> iffT) ##> iffT) (@forall_def (A -> Type)) (@forall_def (B -> Type)).
 Proof.
-  pose (Hfun := biunique_decl_recip1 _ _ _ is_related).
-  pose (Hinj := biunique_decl_recip2 _ _ _ is_related).
+  pose (Hfun := @biunique_decl_recip1 _ _ _ is_related).
+  pose (Hinj := @biunique_decl_recip2 _ _ _ is_related).
   apply bitotal_decl.
   + exact (half_total_predicate R Hfun).
   + intros *.
@@ -274,8 +276,8 @@ Lemma total_predicate_prop
   (is_related : (R ##> R ##> iffT) eq eq) :
   (((R ##> iff) ##> iffT) ##> iffT) (@forall_def (A -> Prop)) (@forall_def (B -> Prop)).
 Proof.
-  pose (Hfun := biunique_decl_recip1 _ _ _ is_related).
-  pose (Hinj := biunique_decl_recip2 _ _ _ is_related).
+  pose (Hfun := @biunique_decl_recip1 _ _ _ is_related).
+  pose (Hinj := @biunique_decl_recip2 _ _ _ is_related).
   apply bitotal_decl.
   + exact (half_total_predicate_prop R Hfun).
   + intros *.
