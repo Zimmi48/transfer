d20fc202e25edd0183fe5b27ebe2ac43ec699fa7
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-04-26 18:13:34 +0200

    Getting rid of subrelations.
    This takes the compile time down from 38s to 22s.

--- theories/Transfer.v
+++ theories/Transfer.v
@@ -27,54 +27,41 @@ Notation " R ##> R' " := (respectful_arrow R R')
 Class Related `(R : A -> B -> Type) (t : A) (t' : B) :=
   is_related : R t t'.
 
-(* Strict subrelation *)
-Class HeteroSubrel `(R : A -> B -> Type) (R' : A -> B -> Type) :=
-  is_heteroSubrel : forall {x y}, R x y -> R' x y.
-
 Tactic Notation "exactm" constr(t) :=
-  exact ((_ : Related impl _ _) t).
+  exact (proj1 (_ : Related iff _ _) t) + exact ((_ : Related impl _ _) t).
 
 Tactic Notation "applym" constr(t) :=
   let H := fresh in
   pose (H := t);
-  apply ((_ : Related impl _ _)) in H;
+  apply (proj1 (_ : Related iff _ _)) + apply ((_ : Related impl _ _)) in H;
   lazy beta delta [ id ] in H;
   apply H;
   clear H.
 
 Tactic Notation "transfer" :=
+  notypeclasses refine (proj1 (_ : Related iff _ _) _) +
   notypeclasses refine ((_ : Related impl _ _) _);
   [ typeclasses eauto |];
   lazy beta delta [ id ].
 
 Tactic Notation "exactm'" constr(t) :=
-  exact ((_ : Related arrow _ _) t).
+  exact (fst (_ : Related iffT _ _) t) + exact ((_ : Related arrow _ _) t).
 
 Tactic Notation "applym'" constr(t) :=
   let H := fresh in
   pose (H := t);
-  apply ((_ : Related arrow _ _)) in H;
+  apply (fst (_ : Related iffT _ _)) + apply ((_ : Related arrow _ _)) in H;
   lazy beta delta [ id ] in H;
   apply H;
   clear H.
 
 Tactic Notation "transfer'" :=
+  notypeclasses refine (fst (_ : Related iffT _ _) _) +
   notypeclasses refine ((_ : Related arrow _ _) _);
   [ typeclasses eauto |];
   lazy beta delta [ id ].
 
 (* RULES *)
-
-(* SUBREL *)
-
-Instance subrel_rule `(R : A -> B -> Type) (R' : A -> B -> Type) (t : A) (t' : B) :
-  HeteroSubrel R R' ->
-  Related R t t' ->
-  Related R' t t' | 9.
-Proof.
-  unfold HeteroSubrel, Related.
-  auto.
-Qed.
 
 (* LAMBDA *)
 
@@ -129,37 +116,6 @@ Instance all_rule (R : Prop -> Prop -> Prop)
 Proof.
   easy.
 Qed.
-
-(* Subrelations *)
-
-Instance sub_impl_arrow : HeteroSubrel impl arrow.
-Proof. easy. Qed.
-
-Instance sub_flip_impl_flip_arrow : HeteroSubrel (flip impl) (flip arrow).
-Proof. easy. Qed.
-
-Instance sub_iff_iffT : HeteroSubrel iff iffT.
-Proof. now intros ? ? []. Qed.
-
-Instance sub_iffT_arrow : HeteroSubrel iffT arrow.
-Proof. now intros ? ? []. Qed.
-
-Instance sub_iffT_flip_arrow : HeteroSubrel iffT (flip arrow).
-Proof. now intros ? ? []. Qed.
-
-Instance sub_iff_impl : HeteroSubrel iff impl.
-Proof. now intros ? ? []. Qed.
-
-Instance sub_iff_flip_impl : HeteroSubrel iff (flip impl).
-Proof. now intros ? ? []. Qed.
-
-Instance sub_respectful_left `(R1 : A -> B -> Type) (R2 : A -> B -> Type) `(R' : C -> D -> Type) :
-  HeteroSubrel R1 R2 -> HeteroSubrel (R2 ##> R') (R1 ##> R').
-Proof. unfold HeteroSubrel, "##>". auto. Qed.
-
-Instance sub_respectful_right `(R : A -> B -> Type) `(R1' : C -> D -> Type) `(R2' : C -> D -> Type) :
-  HeteroSubrel R1' R2' -> HeteroSubrel (R ##> R1') (R ##> R2').
-Proof. unfold HeteroSubrel, "##>". auto. Qed.
 
 (* Predefined instances *)
 
